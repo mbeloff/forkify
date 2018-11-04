ea14c519ee020112d916407c8cf8369a829dbf6a
Michael Beloff <michael.a.beloff@gmail.com>
2018-11-05 09:06:12 +1000

    implement beginning of search functionality

---
+++ src/js/index.js
@@ -0,0 +1,34 @@
+import Search from './models/Search';
+
+/** Global state of the app
+ * Search object
+ * Current recipe object
+ * Shopping list object
+ * Liked recipes
+ */
+
+const state = {};
+
+const controlSearch = async () => {
+  // get query from view
+  const query = 'pizza' // todo
+
+  if (query) {
+    // New search ovject and add to state
+    state.search = new Search(query);
+
+    // Prepare UI for results
+
+    // Search for recipes
+    await state.search.getResults();
+
+    // Render results on UI
+    console.log(state.search.result)
+  }
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+  e.preventDefault();
+  controlSearch();
+})
+
---
+++ src/js/models/Search.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export default class Search {
+  constructor(query) {
+    this.query = query;
+  }
+
+  async getResults(){
+    const key = '5d3a12ed35d871e20d0b1888d58d552e';
+    try {
+      const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+      console.log(res)
+      this.result = res.data.recipes;
+      // console.log(this.result);
+    } catch (error) {
+      alert(error);
+    }    
+  }
+}
