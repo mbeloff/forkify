7c03f8ae741285331d6987ab8cd6664d725dd757
Michael Beloff <michael.a.beloff@gmail.com>
2018-11-08 08:48:42 +1000

    add pagination

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import Search from './models/Search';
 import * as searchView from './views/searchView';
-import { elements } from './views/base';
+import { elements, renderLoader, clearLoader } from './views/base';
 
 /** Global state of the app
  * Search object
@@ -22,11 +22,13 @@ const controlSearch = async () => {
     // Prepare UI for results
     searchView.clearInput();
     searchView.clearResults();
+    renderLoader(elements.searchRes)
     
     // Search for recipes
     await state.search.getResults();
 
     // Render results on UI
+    clearLoader();
     searchView.renderResults(state.search.result);
   }
 }
@@ -35,4 +37,13 @@ elements.searchForm.addEventListener('submit', e => {
   e.preventDefault();
   controlSearch();
 })
+
+elements.searchResPages.addEventListener('click', e => {
+  const btn = e.target.closest('.btn-inline');
+  if (btn) {
+    const goToPage = parseInt(btn.dataset.goto, 10);
+    searchView.clearResults();
+    searchView.renderResults(state.search.result, goToPage);
+  }
+});
 
