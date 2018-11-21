37e042b7ed7a86adc87a92046d87734256b44587
Michael Beloff <michael.a.beloff@gmail.com>
2018-11-22 09:12:20 +1000

    build

--- src/js/index.js
+++ src/js/index.js
@@ -16,7 +16,6 @@ import { elements, renderLoader, clearLoader } from './views/base';
  */
 
 const state = {};
-window.state = state;
 
 /**
  * SEARCH CONTROLLER
@@ -196,5 +195,3 @@ elements.recipe.addEventListener('click', e => {
     controlLike();
   }
 });
-
-window.l = new List();
